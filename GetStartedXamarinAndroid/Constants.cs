cbeacd7201383e0af2c326ba891491ff27b2fb2c
Admin <Admin>
2016-12-15 14:25:32 +0100

    prinsknud

---
+++ GetStartedXamarinAndroid/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GetStartedXamarinAndroid
+{
+
+	public static class Constants
+	{
+    public const string SenderID               = ""; // Google API Project Number
+    public const string ListenConnectionString = "";
+    public const string NotificationHubName = "";
+    }
+}
+
